f17c7a28fbcb3963bc9de40fe01ec217fc0ab4b4
ashiwoo <ashiwoo269@gmail.com>
2026-07-15 19:45:00 +0530

    config-database

---
+++ index.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import sequelize from './config/db.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Agile Sprint Board server is working ' });
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('database is connected');
+
+  app.listen(PORT, () => {
+    console.log(`Server running in port : ${PORT}`);
+  });
+} catch (error) {
+  console.error('database is connection is failed', error.message);
+}
